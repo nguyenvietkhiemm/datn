28097bcc5de9dca5cf2136fc981c88dc8ca90793
Duc-Le-Tri <triduc2k3@gmail.com>
2025-09-19 15:49:01 +0700

    update form

--- src/controllers/role.controller.ts
+++ src/controllers/role.controller.ts
@@ -1,27 +1,36 @@
 import { Request, Response } from 'express';
-import * as roleService from '../services/role.service';
+import roleService from '../services/role.service';
 import safeExcute from '../utils/safe.excute';
 
-export const getAll = async (req: Request, res: Response) => {
-  res.json(await safeExcute(() => roleService.getAllRoles()));
+const roleController = {
+  async getAll(req: Request, res: Response) {
+    const result = await safeExcute(() => roleService.getAllRoles());
+    res.status(result.status).json(result);
+  },
+
+  async getOne(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const result = await safeExcute(() => roleService.getRoleById(id));
+    res.status(result.status).json(result);
+  },
+
+  async create(req: Request, res: Response) {
+    const role = req.body;
+    const result = await safeExcute(() => roleService.createRole(role));
+    res.status(result.status).json(result);
+  },
+
+  async update(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const result = await safeExcute(() => roleService.updateRole(id, req.body));
+    res.status(result.status).json(result);
+  },
+
+  async remove(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const result = await safeExcute(() => roleService.deleteRole(id));
+    res.status(result.status).json(result);
+  }
 };
 
-export const getOne = async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  res.json(await safeExcute(() => roleService.getRoleById(id)))
-};
-
-export const create = async (req: Request, res: Response) => {
-  const role = req.body;
-  res.json(await safeExcute(() => roleService.createRole(role)))
-};
-
-export const update = async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  res.json(await safeExcute(() => roleService.updateRole(id, req.body)));
-};
-
-export const remove = async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  res.json(await safeExcute(() => roleService.deleteRole(id)));
-};
+export default roleController;
