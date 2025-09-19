902937f7bc0ff00f4e434968d162afc8c92e8d26
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2025-09-19 23:41:55 +0700

    question init

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 // routes
 import roleRouter from './role.route';
+import questionRouter from './question.route';
 
 const router = Router();
 
 
 router.use('/roles', roleRouter);
-
+router.use('/question', questionRouter);
 
 export default router;
