9eaf288b3e333c35cc4cedda5e591870a2fccf53
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2025-09-19 16:16:17 +0700

    remove auth.controller.ts

--- src/controllers/auth.controller.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    user_id: number;
-    user_name: string;
-    email: string;
-    password_hash: string;
-    birthday: Date;
-    created_at: Date;
-    role_id: number;
-}
