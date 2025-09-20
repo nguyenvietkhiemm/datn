0544f5b22cea5b21aec855bf974f012f9a02df75
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2025-09-20 19:05:26 +0700

    fix question 2

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -26,19 +26,19 @@ const QuestionController = {
     res.status(201).json(created);
   },
 
-//   async update(req: Request, res: Response) {
-//     const id = Number(req.params.id);
-//     const updated = await RoleService.update(id, req.body);
-//     if (!updated) return res.status(404).json({ message: 'Role not found' });
-//     res.json(updated);
-//   },
+  // async update(req: Request, res: Response) {
+  //   const id = Number(req.params.id);
+  //   const updated = await QuestionService.update(id, req.body);
+  //   if (!updated) return res.status(404).json({ message: 'Role not found' });
+  //   res.json(updated);
+  // },
 
-//   async remove(req: Request, res: Response) {
-//     const id = Number(req.params.id);
-//     const success = await RoleService.remove(id);
-//     if (!success) return res.status(404).json({ message: 'Role not found' });
-//     res.status(204).send();
-//   },
+  // async remove(req: Request, res: Response) {
+  //   const id = Number(req.params.id);
+  //   const success = await RoleService.remove(id);
+  //   if (!success) return res.status(404).json({ message: 'Role not found' });
+  //   res.status(204).send();
+  // },
 };
 
 export default QuestionController;
