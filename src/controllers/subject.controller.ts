28196f64b9dc673c4eca860a6095407cddac348e
LuongHung10 <luongviethung9@gmail.com>
2025-09-22 18:03:24 +0700

    subject init

---
+++ src/controllers/subject.controller.ts
@@ -0,0 +1,51 @@
+import SubjectService from "../services/subject.service";
+import safeExecute, { DefaultResponse } from "../utils/safe.execute";
+import { Request, Response } from "express";
+
+const SubjectController = {
+    async getAll(req: Request, res: Response) {
+        const responses: DefaultResponse<any> = await safeExecute(async () => {
+            return {
+                status: 200,
+                message: "Lấy danh sách môn học thành công",
+                data: await SubjectService.getAll()
+            } as DefaultResponse<any>;
+        });
+        res.json(responses);
+    },
+
+    async create(req: Request, res: Response) {
+        const responses: DefaultResponse<any> = await safeExecute(async () => {
+            return {
+                status: 201,
+                message: "Tạo môn học thành công",
+                data: await SubjectService.create(req.body)
+            } as DefaultResponse<any>;
+        });
+        res.json(responses);
+    },
+
+    async update(req: Request, res: Response) {
+        const responses: DefaultResponse<any> = await safeExecute(async () => {
+            return {
+                status: 202,
+                message: "Cập nhật môn học thành công",
+                data: await SubjectService.update(req.body)
+            } as DefaultResponse<any>;
+        });
+        res.json(responses);
+    },
+
+    async remove(req: Request, res: Response) {
+        const responses: DefaultResponse<any> = await safeExecute(async () => {
+            return {
+                status: 204,
+                message: "Xoá môn học thành công",
+                data: await SubjectService.remove(parseInt(req.params.subject_id, 10))
+            } as DefaultResponse<any>;
+        });
+        res.json(responses);
+    },
+}
+
+export default SubjectController;
