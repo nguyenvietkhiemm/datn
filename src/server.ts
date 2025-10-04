8fed21c9f5cb0d99f71c339c3a142d24c1921ad8
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2025-10-04 20:24:21 +0700

    fix

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, swaggerOptions));
 app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
