7cb5f18da0ce4eebbf1b2414ffc0d19471c725b7
LuongHung10 <luongviethung9@gmail.com>
2025-10-05 10:27:14 +0700

    sửa lại tên hàm trong flashcard, thêm exam question

--- src/model/exam.model.ts
+++ src/model/exam.model.ts
@@ -1,7 +1,10 @@
+import { ExamQuestion} from "./exam.question.model";
+
 export interface Exam {
     exam_id: number;
     title: string;
     create_at: Date;
+    questions: ExamQuestion[];
     time_limit: number; // in minutes
     topic_id: number;
     exam_schedule_id: number;
