7ced56441321200f5bb91985101a8a1c01ad064d
Duc-Le-Tri <triduc2k3@gmail.com>
2025-10-11 13:59:48 +0700

    update flashcard

--- src/utils/create.token.ts
+++ src/utils/create.token.ts
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JSON_TOKEN_KEY || "mysecret";
 
 export default function CreateToken (user_id: number, email: string) {
-  return jwt.sign({ user_id, email }, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign({ user_id, email }, JWT_SECRET, { expiresIn: "1d" });
 };
