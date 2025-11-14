af95bf0c58fec3700eeb446e83d5467628ec1a5b
Duc-Tri-Le <triduc2k3@gmail.com>
2025-11-14 21:24:39 +0700

    fix csv

--- src/controllers/csv.controller.ts
+++ src/controllers/csv.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import safeExecute, { DefaultResponse } from "../utils/safe.execute";
-import { getCsvFilesList, saveCsvFile } from "../services/file.service";
+import { getCsvFilesList, saveCsvFile, getById } from "../services/file.service";
 import path from "path";
 import fs from "fs";
 import { parse } from "csv-parse/sync";
@@ -22,44 +22,23 @@ export const CsvController = {
     },
 
     async getById(req: Request, res: Response) {
-        const result = await safeExecute(async (): Promise<DefaultResponse<any>> => {
+        const result = await safeExecute(async () => {
             const { filename } = req.params;
-            const csvDir = path.join(__dirname, "../../data/uploads/csv");
-
-            if (!fs.existsSync(csvDir)) {
-                throw new Error("Thư mục CSV không tồn tại");
-            }
-
-            const filePath = path.join(csvDir, filename);
-            if (!fs.existsSync(filePath)) {
-                throw new Error("File không tồn tại");
-            }
-
-            const csvText = fs.readFileSync(filePath, "utf-8");
-            const records = parse(csvText, {
-                columns: true,
-                skip_empty_lines: true,
-            });
-
+            const records = getById(filename);
             return {
                 status: 200,
                 message: `Nội dung CSV ${filename}`,
                 data: records,
             };
         });
-
         return res.status(result.status).json(result);
     },
 
     async saveCsv(req: Request, res: Response) {
-        const result = await safeExecute(async (): Promise<DefaultResponse<any>> => {
-            const { filename } = req.params; // tên file CSV
-            // Lấy dữ liệu JSON từ client
-            const data = req.body; // nếu dùng Express, body-parser phải được setup
-
-            if (!Array.isArray(data)) {
-                throw new Error("Dữ liệu phải là mảng object");
-            }
+        const result = await safeExecute(async () => {
+            const { filename } = req.params;
+            const data = req.body;
+            if (!Array.isArray(data)) throw new Error("Dữ liệu phải là mảng object");
 
             saveCsvFile(filename, data);
 
@@ -68,7 +47,6 @@ export const CsvController = {
                 message: `Lưu CSV ${filename} thành công!`,
             };
         });
-
         return res.status(result.status).json(result);
-    }
+    },
 }
