f547b121242ff50b3ee3fd0a56a889fdacf793ae
nguyenvietkhiemm <vietkhiem.nguyen@monkey.edu.vn>
2025-12-20 06:43:34 +0700

    fix document view

--- src/app/document/[id]/page.tsx
+++ src/app/document/[id]/page.tsx
@@ -3,16 +3,23 @@
 import FileView from "@/components/docview/FileView";
 import { useSearchParams } from "next/navigation";
 import styles from "./DocumentView.module.css";
+import {API_URL} from "../../../../lib/service";
 
 export default function MyPage() {
   const searchParams = useSearchParams();
-  const link = searchParams?.get("link") ?? "";
 
-  if (!link) return <p>Không có tài liệu để hiển thị.</p>;
+  const linkParam = searchParams.get("link");
+  const link = linkParam
+    ? `${API_URL}/${linkParam}`
+    : "";
+
+  if (!link) {
+    return <p>Không có tài liệu để hiển thị.</p>;
+  }
 
   return (
     <div className={styles.pageWrapper}>
-      <h1 className={styles.pageTitle}> Preview tài liệu</h1>
+      <h1 className={styles.pageTitle}>Preview tài liệu</h1>
 
       <div className={styles.viewerContainer}>
         <FileView link={link} />
