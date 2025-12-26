5b205c80c3e6f0614db86a32703d7d8687fb2d76
Duc-Tri-Le <triduc2k3@gmail.com>
2025-12-26 20:24:05 +0700

    fix dark

--- lib/ThemeProvider.tsx
+++ lib/ThemeProvider.tsx
@@ -10,22 +10,26 @@ import ProgressPanel from "@/components/panel-progress/page";
 import MiniChat from "@/components/chat-mini/page";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-
     const pathname = usePathname();
     const isDoingExam = pathname.endsWith("/do");
+
     return (
-        <ThemeProvider attribute="class"
-            // defaultTheme="light"
-            enableSystem={false}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"   
+            enableSystem={false}  
+        >
             <ReduxProvider>
-                <div className="flex flex-col min-h-screen">
+                <div className="flex flex-col min-h-screen bg-white text-black">
                     <Header />
+
                     <main className="flex-1 min-h-screen pt-[80px]">
                         <TokenChecker />
                         {children}
                         {!isDoingExam && <MiniChat />}
                         {!isDoingExam && <ProgressPanel />}
                     </main>
+
                     {!isDoingExam && <Footer />}
                 </div>
             </ReduxProvider>
