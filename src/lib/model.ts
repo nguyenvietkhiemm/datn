dbd8c92f28b0daa29de1381148d973de01a384d3
Duc-Tri-Le <triduc2k3@gmail.com>
2026-01-01 15:46:23 +0700

    fix

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -3,18 +3,15 @@ import { Question } from "@/domain/admin/questions/type";
 export function formatVNDateTime(dateInput: string | number | Date): string {
     const date = new Date(dateInput);
 
-    // cộng thêm 7 giờ (UTC+7)
-    const vnTime = new Date(date.getTime() + 7 * 60 * 60 * 1000);
-
     const pad = (n: number) => n.toString().padStart(2, "0");
 
-    const day = pad(vnTime.getDate());
-    const month = pad(vnTime.getMonth() + 1);
-    const year = vnTime.getFullYear();
+    const day = pad(date.getDate());
+    const month = pad(date.getMonth() + 1);
+    const year = date.getFullYear();
 
-    const hours = pad(vnTime.getHours());
-    const minutes = pad(vnTime.getMinutes());
-    const seconds = pad(vnTime.getSeconds());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
 
     return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
