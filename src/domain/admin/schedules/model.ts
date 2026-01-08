4e43f8b43bf24badbba239b8c3e4a5d9c1b0a460
Duc-Tri-Le <triduc2k3@gmail.com>
2026-01-08 19:03:48 +0700

    pussh

--- src/domain/admin/schedules/model.ts
+++ src/domain/admin/schedules/model.ts
@@ -2,13 +2,20 @@ import { ScheduleStatus } from "./type";
 
 export const ScheduleModel = {
     getStatus(start: string, end: string): ScheduleStatus {
-        const now = Date.now();
-        const startTime = new Date(start).getTime();
-        const endTime = new Date(end).getTime();
+        const now = new Date();
+
+        const startTime = new Date(start.replace('Z', ''));
+        const endTime = new Date(end.replace('Z', ''));
+
+        console.log("So sánh thực tế:", {
+            BayGio: now.toLocaleString(),
+            BatDau: startTime.toLocaleString(),
+            KetThuc: endTime.toLocaleString()
+        });
 
         if (now < startTime) return "UPCOMING";
         if (now <= endTime) return "ONGOING";
         return "FINISHED";
     }
-    
+
 };
